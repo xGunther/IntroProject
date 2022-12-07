d854c26069e1bd92d6316101f3412f12d06e9cdd
xGunther <109365935+xGunther@users.noreply.github.com>
2022-12-07 22:16:14 +0100

    Test2

---
+++ Menu Scenes/New_Game_ButtonV4.gd
@@ -0,0 +1,16 @@
+extends Button
+
+export var reference_path = ""
+export (bool) var start_focused = true
+
+func _ready():
+	if(start_focused):
+		grab_focus()
+		connect("Mouse_Entered", self, "Mouse_Enters_Button")
+		connect("pressed", self, "Button_Pressed")
+
+func Mouse_Enters_Button():
+	grab_focus()
+		
+func Button_Pressed():
+		get_tree().change_scene(reference_path)
