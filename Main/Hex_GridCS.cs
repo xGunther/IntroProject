e23fea24658495bb653fc045c4d7bc8c3ad3d871
n1eles <n.m.vandenoever@students.uu.nl>
2022-12-10 18:17:38 +0100

    Proberen Hexa_Tile_Sheep etc. aan te roepen

--- Main/Hex_GridCS.cs
+++ Main/Hex_GridCS.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
+using System.Runtime.ExceptionServices;
 
 public class Hex_GridCS : GridMap
 {
+    private int Tile_Size = 5;
+    var 
+    var texture = ResourceLoader.Load("res://All_Hexa-Tiles/Hexa_Tile_Sheep.tscn") as Texture;
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -10,7 +14,7 @@ public class Hex_GridCS : GridMap
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        make_grid(grid_Range);
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
