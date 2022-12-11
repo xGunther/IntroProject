ac2bad2c939dea9aeb394e342acb979df60c3dee
ThannerJonna <jonathane.vandijk@gmail.com>
2022-12-11 20:54:25 +0100

    Class additions
    Forseeable classes added to prevent reiterating definitions in different classes. WIP as the classes could still change, not definite
    Need to look at removing _Process and possible superclass.

---
+++ All Roads, Cities and Numbers/Road.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+
+//This class will be the blueprint for all other road classes, that belong to different players
+//Therefore, there will be no instances of class
+public abstract class Road : Spatial
+{
+    private string player;//this describes what player this city belongs to
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+    
+    /*// Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+     {
+
+     }*/
+}
