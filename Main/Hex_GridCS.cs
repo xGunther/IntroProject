b0523bf2cda418911a49b0589ea51aa949fbb80e
n1eles <n.m.vandenoever@students.uu.nl>
2022-12-21 18:58:12 +0100

    De .tscn grid files in C# aangeroepen
    + een begin gemaakt voor de grid zelf

--- Main/Hex_GridCS.cs
+++ Main/Hex_GridCS.cs
@@ -4,22 +4,31 @@ using System.Runtime.ExceptionServices;
 
 public class Hex_GridCS : GridMap
 {
-    private int Tile_Size = 5;
-    var 
-    var texture = ResourceLoader.Load("res://All_Hexa-Tiles/Hexa_Tile_Sheep.tscn") as Texture;
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private Texture TileSheep = ResourceLoader.Load("res://All_Hexa-Tiles/Hexa_Tile_Sheep.tscn") as Texture;
+    private Texture TileGold = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Gold.tscn") as Texture;
+    private Texture TileGrain = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Grain.tscn") as Texture;
+    private Texture TileWood = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Wood.tscn") as Texture;
+    private Texture TileStone = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Stone.tscn") as Texture;
+    
+    private int TileSize = 5;
+    private int GridRange = 5; // Number of lines and columns of the grid
+    
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        make_grid(grid_Range);
+        MakeGrid(GridRange);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void MakeGrid(int GridRange)
+    {
+        for (int y = 0; y < GridRange; y++)
+        {
+            Vector2 TileCoords = new Vector2();
+            for (int x = 0; x < GridRange; x++)
+            {
+                var tile = TileSheep.Instance();
+
+            }
+        }
+    }
 }
