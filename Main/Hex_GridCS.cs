151e34f53f702e415f2826df7c9e3b0c930d0012
n1eles <n.m.vandenoever@students.uu.nl>
2022-12-21 21:27:11 +0100

    Test of het project zo runt

--- Main/Hex_GridCS.cs
+++ Main/Hex_GridCS.cs
@@ -4,14 +4,16 @@ using System.Runtime.ExceptionServices;
 
 public class Hex_GridCS : GridMap
 {
-    private Texture TileSheep = ResourceLoader.Load("res://All_Hexa-Tiles/Hexa_Tile_Sheep.tscn") as Texture;
+    //private Texture TileSheep = ResourceLoader.Load("res://All_Hexa-Tiles/Hexa_Tile_Sheep.tscn") as Texture;
     private Texture TileGold = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Gold.tscn") as Texture;
     private Texture TileGrain = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Grain.tscn") as Texture;
     private Texture TileWood = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Wood.tscn") as Texture;
     private Texture TileStone = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Stone.tscn") as Texture;
-    
+
+    private PackedScene Tilesheep = (PackedScene)GD.Load("res://All_Hexa-Tiles/Hexa_Tile_Sheep.tscn");
+
     private int TileSize = 5;
-    private int GridRange = 5; // Number of lines and columns of the grid
+    public int GridRange = 5; // Number of lines and columns of the grid
     
 
     public override void _Ready()
@@ -26,8 +28,8 @@ public class Hex_GridCS : GridMap
             Vector2 TileCoords = new Vector2();
             for (int x = 0; x < GridRange; x++)
             {
-                var tile = TileSheep.Instance();
-
+                Node tile = Tilesheep.Instance();
+                AddChild(tile);
             }
         }
     }
