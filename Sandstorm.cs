1be68f9fd5671b38de78280e8890929e50f25842
ThannerJonna <jonathane.vandijk@gmail.com>
2022-12-21 17:15:10 +0100

    Sandstorm first setup
    Started on:
    Material, sand for particles
    Started on particle-generator
    Maybe aimed a bit high with the particles. Consider changing to a simple mesh

---
+++ Sandstorm.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class Sandstorm : Spatial
+{
+    //Add a public variable that saves the tile it is currently sitting on
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+
+}
