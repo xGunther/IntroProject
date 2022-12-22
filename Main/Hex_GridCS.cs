f1b934b3dd9e1b93c21714e9e6a182338087b268
n1eles <n.m.vandenoever@students.uu.nl>
2022-12-22 22:53:00 +0100

    Added TileChooser and a few comments

--- Main/Hex_GridCS.cs
+++ Main/Hex_GridCS.cs
@@ -1,21 +1,21 @@
 using Godot;
 using System;
+using System.Drawing.Text;
 using System.Runtime.ExceptionServices;
 
 public class Hex_GridCS : GridMap
 {
-    //private Texture TileSheep = ResourceLoader.Load("res://All_Hexa-Tiles/Hexa_Tile_Sheep.tscn") as Texture;
-    private Texture TileGold = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Gold.tscn") as Texture;
-    private Texture TileGrain = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Grain.tscn") as Texture;
-    private Texture TileWood = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Wood.tscn") as Texture;
-    private Texture TileStone = ResourceLoader.Load("res://All_Hexa_Tiles/Hexa_Tile_Stone.tscn") as Texture;
-
+    // Loading the .tscn files into the project
     private PackedScene TileSheep = (PackedScene)GD.Load("res://All_Hexa_Tiles/Hexa_Tile_Sheep.tscn");
+    private PackedScene TileGold = (PackedScene)GD.Load("res://All_Hexa_Tiles/Hexa_Tile_Gold.tscn");
+    private PackedScene TileGrain = (PackedScene)GD.Load("res://All_Hexa_Tiles/Hexa_Tile_Grain.tscn");
+    private PackedScene TileWood = (PackedScene)GD.Load("res://All_Hexa_Tiles/Hexa_Tile_Wood.tscn");
+    private PackedScene TileStone = (PackedScene)GD.Load("res://All_Hexa_Tiles/Hexa_Tile_Stone.tscn");
 
-    private float TileSize = 5;
+    private float TileSize = 5; // Size of the tiles of the board
     public int GridRange = 5; // Number of lines and columns of the grid
 
-    public static float ThirtyDegrees()
+    public static float ThirtyDegrees() // Making a floating number to give to the Vector3 structure
     {
         double radians = (Math.PI / 180) * 30;
         float radiansF = Convert.ToSingle(radians);
@@ -29,15 +29,33 @@ public class Hex_GridCS : GridMap
 
     private void MakeGrid(int GridRange)
     {
+        Vector2 TileCoords;
+        Vector3 TileCoordsV3;
+
+        void TileChooser(int x, int p, int y)
+        {
+            if (x % 2 == 0)
+            {
+                Spatial SheepTile = (Spatial) TileSheep.Instance();
+                AddChild(SheepTile);
+                SheepTile.Translate(TileCoordsV3);
+            }
+            else
+            {
+                Spatial GoldTile = (Spatial) TileGold.Instance();
+                AddChild(GoldTile);
+                GoldTile.Translate(TileCoordsV3);
+            }
+        }
+
         for (int x = 0; x < GridRange; x++)
         {
-            Vector2 TileCoords = new Vector2();
+            TileCoords = new Vector2();
             TileCoords.x = x * TileSize * Mathf.Cos(ThirtyDegrees());
             TileCoords.y = 0;
+            
             for (int y = 0; y < GridRange; y++)
             {
-                Spatial tile = (Spatial) TileSheep.Instance();
-                AddChild(tile);
                 if (x % 2 == 0)
                 {
                     TileCoords.y = y * TileSize;
@@ -46,11 +64,12 @@ public class Hex_GridCS : GridMap
                 {
                     TileCoords.y = (y * TileSize) + TileSize / 2;
                 }
-                Vector3 TileCoordsV3 = new Vector3();
+
+                TileCoordsV3 = new Vector3();
                 TileCoordsV3.x = TileCoords.x;
                 TileCoordsV3.y = 0;
                 TileCoordsV3.z = TileCoords.y;
-                tile.Translate(TileCoordsV3);
+                TileChooser(x, 0, y);
             }
         }
     }
