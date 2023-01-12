c30851ce0d36758889f3cb63b0ca5a98134bf907
ThannerJonna <jonathane.vandijk@gmail.com>
2023-01-12 14:31:41 +0100

    Added buildings list
    A list to keep track of all built placeables and a method where these will be added to the list and the new nodes/placeables will be created. It is in this method that the main parts to building will be created. The method has commenting indicating roughly what happens where.
    Added method where the move will be checked, but that contains no actual checks (always returns true)

--- Main/Builder.cs
+++ Main/Builder.cs
@@ -1,15 +1,35 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 //In this class, all things related to building are added, from creating a new placeable to saving all current placeables
 public class Builder : Node
 {
-    
+    public List<Placeable> AllBuildings= new List<Placeable>(); 
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         
+    }
+
+    //This method will create the nodes/placement instances and add them to the list
+    public void build()
+    {
+        if (MoveCheck())
+        {
+            //make placeable and add it to the list
+        }
+        else
+        {
+            //show a popup that this move is invalid?
+        }
+    }
+
+    //This method will check whether the move is legal or not and return whether it is.
+    private bool MoveCheck()
+    {
+        return true;
     }
 
 }
