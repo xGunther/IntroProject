1dc4413caf90eb515f7258877a47667c5e49fa1a
ThannerJonna <jonathane.vandijk@gmail.com>
2023-01-12 14:19:36 +0100

    Starting building
    Added a builder node where all things related to building will be defined.

---
+++ Main/Builder.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+//In this class, all things related to building are added, from creating a new placeable to saving all current placeables
+public class Builder : Node
+{
+    
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+}
