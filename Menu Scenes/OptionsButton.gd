b03fa8ffe32023ae90cfd97d7b82d08c8c599d92
xGunther <109365935+xGunther@users.noreply.github.com>
2023-01-27 14:30:05 +0100

    Added a HSlider to the titlescreen
    Added a working HSlider to the options menu

---
+++ Menu Scenes/OptionsButton.gd
@@ -0,0 +1,14 @@
+extends Button
+
+#If the mouse enters or the a button is pressed it gives a signal that the mouse has entered or a button is pressed, then it goes to the function that is connected to the signal.
+func _ready():
+	connect("Mouse_Entered", self, "Mouse_Entered")
+	connect("pressed", self, "Button_Pressed")
+	
+#If the mouse enters the button, the button grabs focus. 
+func Mouse_Entered():
+	grab_focus()
+
+#If the button is pressed it will show the options menu
+func Button_Pressed():
+	$"../OptionsMenu".show()
