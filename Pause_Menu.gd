eb325489c8d86e8b5ef88a9843e137ff0a0a2d76
florisyours <97117511+florisyours@users.noreply.github.com>
2023-01-27 22:31:43 +0100

    Current progress 3

--- Pause_Menu.gd
+++ Pause_Menu.gd
@@ -1,16 +1,16 @@
 extends Control
 var reference_path = ""
 
-var Game_is_paused = false setget set_is_paused
+var GameIsPaused = false setget SetIsPaused
 
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
-		self.Game_is_paused = !Game_is_paused
+		self.GameIsPaused = !GameIsPaused
 
-func set_is_paused(value):
-	Game_is_paused = value
-	get_tree().paused = Game_is_paused
-	visible = Game_is_paused
+func SetIsPaused(Value):
+	GameIsPaused = Value
+	get_tree().paused = GameIsPaused
+	visible = GameIsPaused
 
 func _ready():
 	pass 
@@ -20,5 +20,5 @@ func _on_Exit_Button_pressed():
 
 
 func _on_Resum_Button_pressed():
-	self.Game_is_paused = false
+	self.GameIsPaused = false
 
